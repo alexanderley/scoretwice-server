ea6b2cd5f95731da7d5b30f22bc7408e582242df
Filipa Barros <barrosfilipa.f@gmail.com>
2023-07-24 10:07:27 +0200

    models

---
+++ models/Account.model.js
@@ -0,0 +1,115 @@
+const mongoose = require("mongoose");
+
+// Function to generate random IBAN for a specific country code (e.g., DE for Germany)
+function generateRandomIBAN(countryCode) {
+  const randomNumbers = Math.floor(Math.random() * 999999999)
+    .toString()
+    .padStart(9, "0");
+  const iban = `${countryCode}${randomNumbers}`;
+  return iban;
+}
+
+// Function to get a random country code from the list
+function getRandomCountryCode() {
+  const countryCodes = [
+    "AL",
+    "AD",
+    "AT",
+    "AZ",
+    "BH",
+    "BE",
+    "BA",
+    "BR",
+    "BG",
+    "CR",
+    "HR",
+    "CY",
+    "CZ",
+    "DK",
+    "DO",
+    "TL",
+    "EE",
+    "FO",
+    "FI",
+    "FR",
+    "GE",
+    "DE",
+    "GI",
+    "GR",
+    "GL",
+    "GT",
+    "HU",
+    "IS",
+    "IQ",
+    "IE",
+    "IL",
+    "IT",
+    "JO",
+    "KZ",
+    "XK",
+    "KW",
+    "LV",
+    "LB",
+    "LI",
+    "LT",
+    "LU",
+    "MK",
+    "MT",
+    "MR",
+    "MU",
+    "MC",
+    "MD",
+    "ME",
+    "NL",
+    "NO",
+    "PK",
+    "PS",
+    "PL",
+    "PT",
+    "QA",
+    "RO",
+    "LC",
+    "SM",
+    "ST",
+    "SA",
+    "RS",
+    "SC",
+    "SK",
+    "SI",
+    "ES",
+    "SE",
+    "CH",
+    "TN",
+    "TR",
+    "UA",
+    "AE",
+    "GB",
+    "VA",
+    "VG",
+    "UA",
+  ];
+
+  return countryCodes[Math.floor(Math.random() * countryCodes.length)];
+}
+
+const accountSchema = new mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  balance: { type: Number, default: 0 },
+  iban: {
+    type: String,
+    unique: true,
+    required: false,
+  },
+  transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Transaction" }],
+});
+
+// Pre-save hook to generate IBAN if not provided
+accountSchema.pre("save", function (next) {
+  if (!this.iban) {
+    this.iban = generateRandomIBAN(getRandomCountryCode());
+  }
+  next();
+});
+
+const Account = mongoose.model("Account", accountSchema);
+module.exports = Account;
---
+++ models/CreditScore.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+// Define the CreditScore schema
+const creditScoreSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  gender: { type: Boolean, required: true },
+  carOwned: { type: Boolean, required: true },
+  propertyOwned: { type: Boolean, required: true },
+  childrenCount: { type: Number, required: true },
+  annualIncome: { type: Number, required: true },
+  educationLevel: {
+    type: String,
+    enum: [
+      "lower secondary",
+      "secondary",
+      "incomplete higher",
+      "higher education",
+      "academic degree",
+    ],
+    required: true,
+  },
+  maritalStatus: {
+    type: String,
+    enum: [
+      "Married",
+      "Single/ not married",
+      "Separated",
+      "Widow",
+      "Civil marriage",
+    ],
+    required: true,
+  },
+  daysFromEmployment: { type: Date, required: true },
+  ownedWorkphone: { type: Boolean, required: true },
+  ownedEmail: { type: Boolean, required: true },
+  creditStatus: { type: Boolean, required: true },
+
+  // New field for the credit score grade (to be updated later)
+  creditScoreGrade: { type: String, required: false },
+});
+
+// Create the CreditScore model
+const CreditScore = mongoose.model("CreditScore", creditScoreSchema);
+
+// Export the model
+module.exports = CreditScore;
