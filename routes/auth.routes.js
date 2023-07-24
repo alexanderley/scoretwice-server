2bdae8b27b0dcf495ccf47cc7bcb80d8da4d633a
alexanderley <alexander.ley.inbox@gmail.com>
2023-07-24 10:34:29 +0200

    created backup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -124,7 +124,7 @@ router.post("/login", (req, res, next) => {
 router.get("/verify", isAuthenticated, (req, res, next) => {
   // If JWT token is valid the payload gets decoded by the
   // isAuthenticated middleware and made available on `req.payload`
-  console.log(`req.payload verify`, req.payload);
+  // console.log(`req.payload verify`, req.payload);
 
   // Send back the object with user data
   // previously set as the token payload
