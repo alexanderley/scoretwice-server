34a9ae9375f0ccaa9667aa0cd5f6f4557eba833a
Florian Luther <hi@gauvainstudios.com>
2023-07-24 10:36:27 +0200

    new creation account routes

--- db/index.js
+++ db/index.js
@@ -1,16 +1,19 @@
-// ℹ️ package responsible to make the connection with mongodb
-// https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
-
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
-// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const VITE_MONGO_URI =
   process.env.VITE_MONGO_URI ||
   "mongodb://127.0.0.1:27017/project-management-server";
 
 mongoose
-  .connect(VITE_MONGO_URI)
+  .connect(VITE_MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    writeConcern: {
+      w: "majority",
+      j: true,
+      wtimeout: 1000,
+    },
+  })
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
@@ -19,5 +22,3 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
-
-//
