2dd9e78be43bf188a0f573395dfd3c8dd71d7329
Florian Luther <hi@gauvainstudios.com>
2023-07-24 17:00:13 +0200

    transactions post done

--- controllers/transactions.controllers.js
+++ controllers/transactions.controllers.js
@@ -1,48 +1,48 @@
-const asyncHandler = require("express-async-handler");
-const Account = require("../models/Account.model");
-const Transaction = require("../models/Transaction.model");
+// const asyncHandler = require("express-async-handler");
+// const Account = require("../models/Account.model");
+// const Transaction = require("../models/Transaction.model");
 
-// Controller function to add a new transaction
-const addTransaction = asyncHandler(async (req, res) => {
-  const { receiverId, amount } = req.body;
-  const senderId = req.user._id; // Assuming the authenticated user's ID is stored in req.user._id
+// // Controller function to add a new transaction
+// const addTransaction = asyncHandler(async (req, res) => {
+//   const { receiverId, amount } = req.body;
+//   const senderId = req.user._id; // Assuming the authenticated user's ID is stored in req.user._id
 
-  try {
-    // Find the sender and receiver accounts in the database
-    const senderAccount = await Account.findOne({ owner: senderId });
-    const receiverAccount = await Account.findById(receiverId);
+//   try {
+//     // Find the sender and receiver accounts in the database
+//     const senderAccount = await Account.findOne({ owner: senderId });
+//     const receiverAccount = await Account.findById(receiverId);
 
-    if (!senderAccount || !receiverAccount) {
-      res.status(404);
-      throw new Error("Sender or receiver account not found");
-    }
+//     if (!senderAccount || !receiverAccount) {
+//       res.status(404);
+//       throw new Error("Sender or receiver account not found");
+//     }
 
-    // Check if the sender has sufficient balance
-    if (senderAccount.balance < amount) {
-      res.status(400);
-      throw new Error("Insufficient balance");
-    }
+//     // Check if the sender has sufficient balance
+//     if (senderAccount.balance < amount) {
+//       res.status(400);
+//       throw new Error("Insufficient balance");
+//     }
 
-    // Deduct the amount from the sender's account
-    senderAccount.balance -= amount;
-    await senderAccount.save();
+//     // Deduct the amount from the sender's account
+//     senderAccount.balance -= amount;
+//     await senderAccount.save();
 
-    // Add the amount to the receiver's account
-    receiverAccount.balance += amount;
-    await receiverAccount.save();
+//     // Add the amount to the receiver's account
+//     receiverAccount.balance += amount;
+//     await receiverAccount.save();
 
-    // Create a new transaction record
-    const transaction = new Transaction({
-      sender: senderId,
-      receiver: receiverId,
-      amount,
-    });
-    await transaction.save();
+//     // Create a new transaction record
+//     const transaction = new Transaction({
+//       sender: senderId,
+//       receiver: receiverId,
+//       amount,
+//     });
+//     await transaction.save();
 
-    res.status(200).json({ message: "Transaction successful" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+//     res.status(200).json({ message: "Transaction successful" });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
-module.exports = { addTransaction };
+// module.exports = { addTransaction };
--- routes/transactions.routes.js
+++ routes/transactions.routes.js
@@ -1,9 +1,69 @@
 const express = require("express");
-const router = express.Router();
-const { isAuthenticated } = require("../middleware/jwt.middleware");
-const { addTransaction } = require("../controllers/transactions.controllers");
+const mongoose = require("mongoose");
+const User = require("../models/User.model");
+const Account = require("../models/Account.model");
+const Transaction = require("../models/Transaction.model");
 
-// POST route to initiate a new transaction
-router.post("/transactions", isAuthenticated, addTransaction);
+const app = express();
+app.use(express.json());
+const router = express.Router();
+
+router.post("/users/:senderId/transactions/:receiverId", (req, res) => {
+  const senderId = req.params.senderId;
+  const receiverId = req.params.receiverId;
+  const amount = req.body.amount;
+
+  // Find the sender and receiver users and their accounts
+  Promise.all([
+    User.findById(senderId).populate("account"),
+    User.findById(receiverId).populate("account"),
+  ])
+    .then(([sender, receiver]) => {
+      if (!sender || !receiver) {
+        return res.status(404).json({ error: "One or both users not found." });
+      }
+
+      if (!sender.account || !receiver.account) {
+        return res
+          .status(404)
+          .json({ error: "One or both users do not have an account." });
+      }
+
+      if (sender.account.balance < amount) {
+        return res
+          .status(400)
+          .json({ error: "Insufficient balance for the transaction." });
+      }
+
+      // Create a new transaction
+      const transaction = new Transaction({
+        sender: senderId,
+        receiver: receiverId,
+        amount: amount,
+      });
+
+      // Update account balances
+      sender.account.balance -= amount;
+      receiver.account.balance += amount;
+
+      return Promise.all([
+        transaction.save(),
+        sender.account.save(),
+        receiver.account.save(),
+      ]);
+    })
+    .then(([transaction, senderAccount, receiverAccount]) => {
+      return res.json({
+        message: "Transaction successful.",
+        transaction: transaction,
+        senderNewBalance: senderAccount.balance,
+        receiverNewBalance: receiverAccount.balance,
+      });
+    })
+    .catch((err) => {
+      console.error("Error while processing transaction:", err);
+      return res.status(500).json({ error: "Internal server error." });
+    });
+});
 
 module.exports = router;
